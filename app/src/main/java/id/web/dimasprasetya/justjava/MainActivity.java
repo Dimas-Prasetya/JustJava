6593b7df2ebb8d04e2d1a8e1445efe06c8d2be9f
Dimas Prasetya <prasetya.dimas95@gmail.com>
2017-03-19 22:46:34 +0700

    Add Name Edit Text & Update Order Summary

--- app/src/main/java/id/web/dimasprasetya/justjava/MainActivity.java
+++ app/src/main/java/id/web/dimasprasetya/justjava/MainActivity.java
@@ -5,9 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,29 +38,47 @@ public class MainActivity extends AppCompatActivity {
 
     // submit order
     public void submitOrder(View view) {
+        EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
+        String valueName = nameEditText.getText().toString();
+        // Log.v("MainActivity", "Name : " + valueName);
+
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWippedCream = whippedCreamCheckBox.isChecked();
-        Log.v("MainActivity", "Has wipped cream : " + hasWippedCream);
+        // Log.v("MainActivity", "Has wipped cream : " + hasWippedCream);
 
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_check_box);
         boolean hasChocolate = chocolateCheckBox.isChecked();
-        Log.v("MainAvticity", "Has chocolate : " + hasChocolate);
+        // Log.v("MainAvticity", "Has chocolate : " + hasChocolate);
 
         int cost = calculatePrice(quantity, price);
-        String priceMessage = createOrderSummary(cost, hasWippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(valueName, hasWippedCream, hasChocolate, cost);
         displayMessage(priceMessage);
     }
 
+    /**
+     *
+     * @param quantity      = quantity of coffee
+     * @param pricePerCup   = price per cup of coffee
+     * @return
+     */
     private int calculatePrice(int quantity, int pricePerCup) {
         return quantity * pricePerCup;
     }
 
-    private String createOrderSummary(int cost, boolean addWhippedCream, boolean addChocolate) {
-        String priceMessage = "Name     : Dimas Prasetya";
+    /**
+     *
+     * @param addValueName      = add customer's name
+     * @param addWhippedCream   = add whipped cream topping
+     * @param addChocolate      = add chocolate cream topping
+     * @param cost              = calculate the price
+     * @return
+     */
+    private String createOrderSummary(String addValueName, boolean addWhippedCream, boolean addChocolate, int cost) {
+        String priceMessage = "Name     : " + addValueName;
         priceMessage += "\nAdd whipped cream ? " + addWhippedCream;
         priceMessage += "\nAdd chocolate ? " + addChocolate;
         priceMessage += "\nQuantity : " + quantity;
-        priceMessage += "\nTotal    : " + cost;
+        priceMessage += "\nTotal       : " + cost;
         priceMessage += "\nThank You!";
         return priceMessage;
     }
@@ -69,24 +86,6 @@ public class MainActivity extends AppCompatActivity {
     public void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.valueOrderSummary_text_view);
         orderSummaryTextView.setText(message);
-    }
-
-    // cancel order
-    public void cancelOrder(View view) {
-        int quantity = 0;
-        int price = 0;
-        cancel(quantity);
-        cancelPrice(price);
-    }
-
-    public void cancel(int number) {
-        TextView valueTextView = (TextView) findViewById(R.id.value_text_view);
-        valueTextView.setText("" + number);
-    }
-
-    public void cancelPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.valueOrderSummary_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
 }
